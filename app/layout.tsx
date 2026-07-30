02823a882c8a4526db85348624f27ada113f4584
Allan Mathenge <allanmathenge67@gmail.com>
2026-07-30 13:33:48 +0300

    removed manifest link from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,9 +64,7 @@ export default function RootLayout({
       <head>
         {/* Bing validation */}
         <meta name="msvalidate.01" content="7F9BEC1255ABF3C4802D7356DC131BE7" />
-        
-        {/* PWA: Manifest */}
-        <link rel="manifest" href="/manifest.json" />
+    
         
         {/* PWA: Apple Touch Icon */}
         <link rel="apple-touch-icon" href="/icon-192.png" />
