135bb0444101859a34fc049a5a834055ac937262
Allan Mathenge <allanmathenge67@gmail.com>
2026-08-13 16:51:40 +0300

    update dashboard layout

--- components/dashboard/DashboardLayoutClient.tsx
+++ components/dashboard/DashboardLayoutClient.tsx
@@ -29,7 +29,7 @@ export function DashboardLayoutClient({ children, role }: DashboardLayoutClientP
       />
 
       {/* Main Content */}
-      <main className="flex-1 overflow-y-auto p-4 md:p-6 space-y-6">
+      <main className="flex-1 overflow-y-auto p-4 md:p-12 space-y-6">
         {children}
       </main>
     </div>
